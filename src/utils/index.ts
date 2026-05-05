aca0953a1faa469425cc7a50b4210d2c8d7448a9
Marcel Castro <marcel.castro@snowflake.com>
2026-05-05 23:56:04 +0200

    Refactor retry utility to use options object

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { generateBranchName } from "./branch";
 export type { BranchNameOptions } from "./branch";
 export { sanitizeContent } from "./sanitize";
 export { withRetry } from "./retry";
+export type { RetryOptions } from "./retry";
